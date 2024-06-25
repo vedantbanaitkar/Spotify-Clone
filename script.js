afed97f88c65cd64e059c2f682b9042e25ed22e1
Vedant Banaitkar <vedantbanaitkar@gmail.com>
2024-06-26 01:41:29 +0530

    Day - 01
    Partially Done with UI and functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+}
+
+async function main() {
+  let songs = await getSongs();
+  console.log(songs)
+
+  //play first song
+//   let audio = new Audio(songs[0]);
+//   audio.play();
+}
+
+main();
